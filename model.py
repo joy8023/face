9c26501be46c7dd00238e06f95b10ee4a9eabab1
joy8023 <joyjoy8023@gmail.com>
2021-08-04 23:00:41 -0400

    init

---
+++ model.py
@@ -0,0 +1,319 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class conv2DBatchNormRelu(nn.Module):
+    def __init__(
+            self,
+            in_channels,
+            n_filters,
+            k_size,
+            stride,
+            padding,
+            bias=True,
+            dilation=1,
+            with_bn=True,
+    ):
+        super(conv2DBatchNormRelu, self).__init__()
+
+        conv_mod = nn.Conv2d(int(in_channels),
+                             int(n_filters),
+                             kernel_size=k_size,
+                             padding=padding,
+                             stride=stride,
+                             bias=bias,
+                             dilation=dilation, )
+
+        if with_bn:
+            self.cbr_unit = nn.Sequential(conv_mod,
+                                          nn.BatchNorm2d(int(n_filters)),
+                                          nn.ReLU(inplace=True))
+        else:
+            self.cbr_unit = nn.Sequential(conv_mod, nn.ReLU(inplace=True))
+
+    def forward(self, inputs):
+        outputs = self.cbr_unit(inputs)
+        return outputs
+
+
+class segnetDown2(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(segnetDown2, self).__init__()
+        self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+        self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        unpooled_shape = outputs.size()
+        outputs, indices = self.maxpool_with_argmax(outputs)
+        return outputs, indices, unpooled_shape
+
+
+class segnetDown3(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(segnetDown3, self).__init__()
+        self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+        self.conv3 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+        self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        outputs = self.conv3(outputs)
+        unpooled_shape = outputs.size()
+        outputs, indices = self.maxpool_with_argmax(outputs)
+        return outputs, indices, unpooled_shape
+
+
+class segnetUp2(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(segnetUp2, self).__init__()
+        self.unpool = nn.MaxUnpool2d(2, 2)
+        self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+
+    def forward(self, inputs, indices, output_shape):
+        outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
+        outputs = self.conv1(outputs)
+        outputs = self.conv2(outputs)
+        return outputs
+
+
+class segnetUp3(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(segnetUp3, self).__init__()
+        self.unpool = nn.MaxUnpool2d(2, 2)
+        self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+        self.conv3 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+
+    def forward(self, inputs, indices, output_shape):
+        outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
+        outputs = self.conv1(outputs)
+        outputs = self.conv2(outputs)
+        outputs = self.conv3(outputs)
+        return outputs
+
+
+class SegNet(nn.Module):
+    def __init__(self, n_classes=3, in_channels=3, is_unpooling=True):
+        super(SegNet, self).__init__()
+
+        self.in_channels = in_channels
+        self.is_unpooling = is_unpooling
+
+        self.down1 = segnetDown2(self.in_channels, 64)
+        self.down2 = segnetDown2(64, 128)
+        self.down3 = segnetDown3(128, 256)
+        self.down4 = segnetDown3(256, 512)
+        self.down5 = segnetDown3(512, 512)
+
+        self.up5 = segnetUp3(512, 512)
+        self.up4 = segnetUp3(512, 256)
+        self.up3 = segnetUp3(256, 128)
+        self.up2 = segnetUp2(128, 64)
+        self.up1 = segnetUp2(64, n_classes)
+
+    def forward(self, inputs):
+
+        down1, indices_1, unpool_shape1 = self.down1(inputs)
+        down2, indices_2, unpool_shape2 = self.down2(down1)
+        down3, indices_3, unpool_shape3 = self.down3(down2)
+        down4, indices_4, unpool_shape4 = self.down4(down3)
+        down5, indices_5, unpool_shape5 = self.down5(down4)
+
+        up5 = self.up5(down5, indices_5, unpool_shape5)
+        up4 = self.up4(up5, indices_4, unpool_shape4)
+        up3 = self.up3(up4, indices_3, unpool_shape3)
+        up2 = self.up2(up3, indices_2, unpool_shape2)
+        up1 = self.up1(up2, indices_1, unpool_shape1)
+
+        return up1
+
+    def init_vgg16_params(self, vgg16):
+        blocks = [self.down1, self.down2, self.down3, self.down4, self.down5]
+
+        ranges = [[0, 4], [5, 9], [10, 16], [17, 23], [24, 29]]
+        features = list(vgg16.features.children())
+
+        vgg_layers = []
+        for _layer in features:
+            if isinstance(_layer, nn.Conv2d):
+                vgg_layers.append(_layer)
+
+        merged_layers = []
+        for idx, conv_block in enumerate(blocks):
+            if idx < 2:
+                units = [conv_block.conv1.cbr_unit, conv_block.conv2.cbr_unit]
+            else:
+                units = [
+                    conv_block.conv1.cbr_unit,
+                    conv_block.conv2.cbr_unit,
+                    conv_block.conv3.cbr_unit,
+                ]
+            for _unit in units:
+                for _layer in _unit:
+                    if isinstance(_layer, nn.Conv2d):
+                        merged_layers.append(_layer)
+
+        assert len(vgg_layers) == len(merged_layers)
+
+        for l1, l2 in zip(vgg_layers, merged_layers):
+            if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
+                assert l1.weight.size() == l2.weight.size()
+                assert l1.bias.size() == l2.bias.size()
+                l2.weight.data = l1.weight.data
+                l2.bias.data = l1.bias.data
+
+
+
+
+class Classifier(nn.Module):
+    def __init__(self, nc, ndf, nz):
+        super(Classifier, self).__init__()
+
+        self.nc = nc
+        self.ndf = ndf
+        self.nz = nz
+
+        self.encoder = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 3, 1, 1),
+            nn.BatchNorm2d(ndf),
+            nn.MaxPool2d(2, 2, 0),
+            nn.ReLU(True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 3, 1, 1),
+            nn.BatchNorm2d(ndf * 2),
+            nn.MaxPool2d(2, 2, 0),
+            nn.ReLU(True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 3, 1, 1),
+            nn.BatchNorm2d(ndf * 4),
+            nn.MaxPool2d(2, 2, 0),
+            nn.ReLU(True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 3, 1, 1),
+            nn.BatchNorm2d(ndf * 8),
+            nn.MaxPool2d(2, 2, 0),
+            nn.ReLU(True),
+            # state size. (ndf*8) x 4 x 4
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(ndf * 8 * 4 * 4, nz * 5),
+            nn.Dropout(0.5),
+            nn.Linear(nz * 5, nz),
+        )
+
+    def forward(self, x, logit = False, log = True):
+
+        x = x.view(-1, 1, 64, 64)
+        x = self.encoder(x)
+        x = x.view(-1, self.ndf * 8 * 4 * 4)
+        x = self.fc(x)
+
+        if logit:
+            return x
+
+        if log:
+            return F.log_softmax(x, dim=1)
+        else:
+            #return F.log_softmax(x, dim=1)
+            return F.softmax(x, dim=1)
+
+class Inversion(nn.Module):
+    def __init__(self, nc, ngf, nz, truncation, c):
+        super(Inversion, self).__init__()
+
+        self.nc = nc
+        self.ngf = ngf
+        self.nz = nz
+        self.truncation = truncation
+        self.c = c
+
+        self.decoder = nn.Sequential(
+            # input is Z
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0),
+            nn.BatchNorm2d(ngf * 8),
+            nn.Tanh(),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1),
+            nn.BatchNorm2d(ngf * 4),
+            nn.Tanh(),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1),
+            nn.BatchNorm2d(ngf * 2),
+            nn.Tanh(),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1),
+            nn.BatchNorm2d(ngf),
+            nn.Tanh(),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1),
+            nn.Sigmoid()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, x):
+        topk, indices = torch.topk(x, self.truncation)
+        topk = torch.clamp(torch.log(topk), min=-1000) + self.c
+        topk_min = topk.min(1, keepdim=True)[0]
+        topk = topk + F.relu(-topk_min)
+        x = torch.zeros(len(x), self.nz).cuda().scatter_(1, indices, topk)
+
+        x = x.view(-1, self.nz, 1, 1)
+        x = self.decoder(x)
+        x = x.view(-1, 1, 64, 64)
+        return x
+
+
+class Inversion(nn.Module):
+    def __init__(self, nc, ngf, nz, truncation, c):
+        super(Inversion, self).__init__()
+
+        self.nc = nc
+        self.ngf = ngf
+        self.nz = nz
+        self.truncation = truncation
+        self.c = c
+
+        self.decoder = nn.Sequential(
+            # input is Z
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0),
+            nn.BatchNorm2d(ngf * 8),
+            nn.Tanh(),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1),
+            nn.BatchNorm2d(ngf * 4),
+            nn.Tanh(),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1),
+            nn.BatchNorm2d(ngf * 2),
+            nn.Tanh(),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1),
+            nn.BatchNorm2d(ngf),
+            nn.Tanh(),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1),
+            nn.Sigmoid()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, x):
+        topk, indices = torch.topk(x, self.truncation)
+        topk = torch.clamp(torch.log(topk), min=-1000) + self.c
+        topk_min = topk.min(1, keepdim=True)[0]
+        topk = topk + F.relu(-topk_min)
+        x = torch.zeros(len(x), self.nz).cuda().scatter_(1, indices, topk)
+
+        x = x.view(-1, self.nz, 1, 1)
+        x = self.decoder(x)
+        x = x.view(-1, 1, 64, 64)
+        return x
---
+++ train.py
@@ -0,0 +1,221 @@
+import time
+
+import torch.optim as optim
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from model import SegNet
+from data import FaceScrub
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+input_nbr = 3
+imsize = 112
+batch_size = 32
+lr = 0.0001
+patience = 50
+start_epoch = 0
+epochs = 100
+print_freq = 10
+save_folder = 'models'
+
+def ensure_folder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
+def adjust_learning_rate(optimizer, shrink_factor):
+    print("\nDECAYING learning rate.")
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = param_group['lr'] * shrink_factor
+    print("The new learning rate is %f\n" % (optimizer.param_groups[0]['lr'],))
+
+
+class ExpoAverageMeter(object):
+    # Exponential Weighted Average Meter
+    def __init__(self, beta=0.9):
+        self.reset()
+
+    def reset(self):
+        self.beta = 0.9
+        self.val = 0
+        self.avg = 0
+        self.count = 0
+
+    def update(self, val):
+        self.val = val
+        self.avg = self.beta * self.avg + (1 - self.beta) * self.val
+
+
+def save_checkpoint(epoch, model, optimizer, val_loss, is_best):
+    ensure_folder(save_folder)
+    state = {'model': model,
+             'optimizer': optimizer}
+    filename = '{0}/checkpoint_{1}_{2:.3f}.tar'.format(save_folder, epoch, val_loss)
+    torch.save(state, filename)
+    # If this checkpoint is the best so far, store a copy so it doesn't get overwritten by a worse checkpoint
+    if is_best:
+        torch.save(state, '{}/BEST_checkpoint.tar'.format(save_folder))
+
+
+def train(epoch, train_loader, model, optimizer):
+    # Ensure dropout layers are in train mode
+    model.train()
+
+    # Loss function
+    # criterion = nn.MSELoss().to(device)
+
+    batch_time = ExpoAverageMeter()  # forward prop. + back prop. time
+    losses = ExpoAverageMeter()  # loss (per word decoded)
+
+    start = time.time()
+
+    # Batches
+    for i_batch, (x, y) in enumerate(train_loader):
+        # Set device options
+        x = x.to(device)
+        y = y.to(device)
+
+        # print('x.size(): ' + str(x.size())) # [32, 3, 224, 224]
+        # print('y.size(): ' + str(y.size())) # [32, 3, 224, 224]
+
+        # Zero gradients
+        optimizer.zero_grad()
+
+        y_hat = model(x)
+        # print('y_hat.size(): ' + str(y_hat.size())) # [32, 3, 224, 224]
+
+        loss = torch.sqrt((y_hat - y).pow(2).mean())
+        loss.backward()
+
+        # def closure():
+        #     optimizer.zero_grad()
+        #     y_hat = model(x)
+        #     loss = torch.sqrt((y_hat - y).pow(2).mean())
+        #     loss.backward()
+        #     losses.update(loss.item())
+        #     return loss
+
+        # optimizer.step(closure)
+        optimizer.step()
+
+        # Keep track of metrics
+        losses.update(loss.item())
+        batch_time.update(time.time() - start)
+
+        start = time.time()
+
+        # Print status
+        if i_batch % print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(epoch, i_batch, len(train_loader),
+                                                                  batch_time=batch_time,
+                                                                  loss=losses))
+
+
+def valid(val_loader, model):
+    model.eval()  # eval mode (no dropout or batchnorm)
+
+    # Loss function
+    # criterion = nn.MSELoss().to(device)
+
+    batch_time = ExpoAverageMeter()  # forward prop. + back prop. time
+    losses = ExpoAverageMeter()  # loss (per word decoded)
+
+    start = time.time()
+    plot = True
+    ensure_folder('out')
+
+    with torch.no_grad():
+        # Batches
+        for i_batch, (x, y) in enumerate(val_loader):
+            # Set device options
+            x = x.to(device)
+            y = y.to(device)
+
+            y_hat = model(x)
+
+            loss = torch.sqrt((y_hat - y).pow(2).mean())
+
+            # Keep track of metrics
+            losses.update(loss.item())
+            batch_time.update(time.time() - start)
+
+            start = time.time()
+
+            # Print status
+            if i_batch % print_freq == 0:
+                print('Validation: [{0}/{1}]\t'
+                      'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(i_batch, len(val_loader),
+                                                                      batch_time=batch_time,
+                                                                      loss=losses))
+            if plot:
+                truth = data[0:32]
+                inverse = reconstruction[0:32]
+                out = torch.cat((inverse, truth))
+                for i in range(4):
+                    out[i * 16:i * 16 + 8] = inverse[i * 8:i * 8 + 8]
+                    out[i * 16 + 8:i * 16 + 16] = truth[i * 8:i * 8 + 8]
+                vutils.save_image(out, 'out/recon_{}_{}.png'.format(msg.replace(" ", ""), epoch), normalize=False)
+                plot = False
+
+    return losses.avg
+
+
+def main():
+
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_set = FaceScrub('./face.npz', transform=transform)
+    test_set = FaceScrub('./face_test.npz', transform=transform)
+
+    # Create SegNet model
+    label_nbr = 3
+    model = SegNet(label_nbr)
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        # dim = 0 [40, xxx] -> [10, ...], [10, ...], [10, ...], [10, ...] on 4 GPUs
+        model = nn.DataParallel(model)
+    # Use appropriate device
+    model = model.to(device)
+    # print(model)
+
+    # define the optimizer
+    # optimizer = optim.LBFGS(model.parameters(), lr=0.8)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    best_loss = 100000
+    epochs_since_improvement = 0
+
+    # Epochs
+    for epoch in range(start_epoch, epochs):
+        # Decay learning rate if there is no improvement for 8 consecutive epochs, and terminate training after 20
+        if epochs_since_improvement == 20:
+            break
+        if epochs_since_improvement > 0 and epochs_since_improvement % 8 == 0:
+            adjust_learning_rate(optimizer, 0.8)
+
+        # One epoch's training
+        train(epoch, train_loader, model, optimizer)
+
+        # One epoch's validation
+        val_loss = valid(val_loader, model)
+        print('\n * LOSS - {loss:.3f}\n'.format(loss=val_loss))
+
+        # Check if there was an improvement
+        is_best = val_loss < best_loss
+        best_loss = min(best_loss, val_loss)
+
+        if not is_best:
+            epochs_since_improvement += 1
+            print("\nEpochs since last improvement: %d\n" % (epochs_since_improvement,))
+        else:
+            epochs_since_improvement = 0
+
+        # Save checkpoint
+        save_checkpoint(epoch, model, optimizer, val_loss, is_best)
+
+
+if __name__ == '__main__':
+    main()
